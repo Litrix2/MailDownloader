8b40191124e9841a00192a4402cd7954320b557f
Litrix2 <wbpstn@163.com>
2023-01-17 21:23:40 +0800

    feat: 更新版本 1.2.4
    更新版本 1.2.4
    
    BREAKING CHANGE: 配置文件必须更新

--- mail_downloader.py
+++ mail_downloader.py
@@ -15,7 +15,7 @@ import time
 import traceback
 import urllib.parse
 
-version = '1.2.3'
+version = '1.2.4'
 mode = 0  # 0:Release;1:Alpha;2:Beta;3:Demo
 authentication = ['name', 'MailDownloader', 'version', version]
 available_bigfile_website_list = [
