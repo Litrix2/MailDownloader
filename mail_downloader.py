f7dcede96ca23e3165c866bb816b4f48447097d3
Litrix2 <wbpstn@163.com>
2023-01-20 07:04:39 +0800

    fix: 现在时区转换错误的时间会按原样显示,而不是报错

--- mail_downloader.py
+++ mail_downloader.py
@@ -692,21 +692,21 @@ def download_thread_func(thread_id):
                             data_msg_raw[0][1])
                         subject = str(header.make_header(
                             header.decode_header(data_msg.get('Subject'))))
-                        send_time = str(header.make_header(
+                        send_time_raw = str(header.make_header(
                             header.decode_header(data_msg.get('Date'))))[5:]
                         try:
-                            if '(' in send_time:
-                                send_time = send_time[:send_time.find(' (')]
-                            if '+' in send_time or '-' in send_time:
+                            if '(' in send_time_raw:
+                                send_time = send_time_raw[:send_time_raw.find(' (')]
+                            if '+' in send_time_raw or '-' in send_time_raw:
                                 send_time = str(datetime.datetime.strptime(
-                                send_time, '%d %b %Y %H:%M:%S %z').astimezone(pytz.timezone('Etc/GMT-8')))[:-6]
-                            elif 'GMT' in send_time:
+                                send_time_raw, '%d %b %Y %H:%M:%S %z').astimezone(pytz.timezone('Etc/GMT-8')))[:-6]
+                            elif 'GMT' in send_time_raw:
                                 send_time = str(datetime.datetime.strptime(
-                                send_time, '%d %b %Y %H:%M:%S %Z').astimezone(pytz.timezone('Etc/GMT+8')))[:-6]
+                                send_time_raw, '%d %b %Y %H:%M:%S %Z').astimezone(pytz.timezone('Etc/GMT+8')))[:-6]
                             else:
                                 raise ValueError
                         except ValueError:
-                            send_time='时间获取错误.'
+                            send_time=send_time_raw
                         try:
                             for eachdata_msg in data_msg.walk():
                                 file_name = None
